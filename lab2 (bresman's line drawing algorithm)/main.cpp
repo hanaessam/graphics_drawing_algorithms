7f88da25fabf5200a8e6fdd724241439a5622d9f
hanaessam <hanaessamsayed@gmail.com>
2023-03-20 12:57:07 +0200

    namings & few tweaks

--- lab2 (bresman's line drawing algorithm)/main.cpp
+++ lab2 (bresman's line drawing algorithm)/main.cpp
@@ -83,7 +83,7 @@ void drawLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color)
     int dy = abs(y2 - y1);
     int sx = (x1 < x2) ? 1 : -1;
     int sy = (y1 < y2) ? 1 : -1;
-    int err = dx - dy;
+    int change = dx - dy;
 
     while (true)
     {
@@ -94,17 +94,17 @@ void drawLine(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color)
             break;
         }
 
-        int e2 = 2 * err;
+        int change2 = 2 * change;
 
-        if (e2 > -dy)
+        if (change2 > -dy)
         {
-            err -= dy;
+            change -= dy;
             x1 += sx;
         }
 
-        if (e2 < dx)
+        if (change2 < dx)
         {
-            err += dx;
+            change += dx;
             y1 += sy;
         }
     }
