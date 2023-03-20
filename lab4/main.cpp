f8f5c457350bda0588328d3e38788509f3f52192
hanaessam <hanaessamsayed@gmail.com>
2023-03-20 12:51:29 +0200

    first commit

---
+++ lab4/main.cpp
@@ -0,0 +1,147 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+#define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+#define UNICODE
+#endif
+
+#include <tchar.h>
+#include <windows.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+
+/*  Make the class name into a global variable  */
+TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nCmdShow)
+{
+    HWND hwnd;               /* This is the handle for our window */
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* The Window structure */
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    wincl.cbSize = sizeof (WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
+    wincl.cbWndExtra = 0;                      /* structure or the window instance */
+    /* Use Windows's default colour as the background of the window */
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    /* The class is registered, let's create the program*/
+    hwnd = CreateWindowEx (
+               0,                   /* Extended possibilites for variation */
+               szClassName,         /* Classname */
+               _T("Code::Blocks Template Windows App"),       /* Title Text */
+               WS_OVERLAPPEDWINDOW, /* default window */
+               CW_USEDEFAULT,       /* Windows decides the position */
+               CW_USEDEFAULT,       /* where the window ends up on the screen */
+               1000,                 /* The programs width */
+               1000,                 /* and height in pixels */
+               HWND_DESKTOP,        /* The window is a child-window to desktop */
+               NULL,                /* No menu */
+               hThisInstance,       /* Program Instance handler */
+               NULL                 /* No Window Creation data */
+           );
+
+    /* Make the window visible on the screen */
+    ShowWindow (hwnd, nCmdShow);
+
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+    }
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+
+
+void Draw8Points(HDC hdc,int xc,int yc, int a, int b,COLORREF color)
+{
+    SetPixel(hdc, xc+a, yc+b, color);
+    SetPixel(hdc, xc-a, yc+b, color);
+    SetPixel(hdc, xc-a, yc-b, color);
+    SetPixel(hdc, xc+a, yc-b, color);
+    SetPixel(hdc, xc+b, yc+a, color);
+    SetPixel(hdc, xc-b, yc+a, color);
+    SetPixel(hdc, xc-b, yc-a, color);
+    SetPixel(hdc, xc+b, yc-a, color);
+}
+
+void CircleDirect(HDC hdc,int xc,int yc, int R,COLORREF color)
+{
+    int x=0,y=R;
+    int R2=R*R;
+    Draw8Points(hdc,xc,yc,x,y,color);
+    while(x<y)
+    {
+        x++;
+        y=round(sqrt((double)(R2-x*x)));
+        Draw8Points(hdc,xc,yc,x,y,color);
+    }
+}
+
+
+
+/*  This function is called by the Windows function DispatchMessage()  */
+
+LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+    static int x1, y1, x2, y2;
+    static bool isDrawing = false;
+    HDC hdc = GetDC(hwnd);
+    COLORREF color = RGB(0,15,20);
+    switch (message)                  /* handle the messages */
+    {
+    case WM_LBUTTONDOWN:
+        if (!isDrawing)
+        {
+            x1 = LOWORD(lParam);
+            y1 = HIWORD(lParam);
+            x2 = x1;
+            y2 = y1;
+            isDrawing = true;
+        }
+        else
+        {
+            x2 = LOWORD(lParam);
+            y2 = HIWORD(lParam);
+            int radius = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            CircleDirect(hdc, x1, y1, radius, color);
+            isDrawing = false;
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+        break;
+    default:                      /* for messages that we don't deal with */
+        return DefWindowProc (hwnd, message, wParam, lParam);
+    }
+
+    return 0;
+}
