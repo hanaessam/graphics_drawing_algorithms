3cde88fbaf262a6c0c726b9cb5f12e11b741017a
hanaessam <hanaessamsayed@gmail.com>
2023-04-26 21:58:23 +0200

    clipping

---
+++ lab6 (line clipping)/main.cpp
@@ -0,0 +1,185 @@
+#if defined(UNICODE) && !defined(_UNICODE)
+#define _UNICODE
+#elif defined(_UNICODE) && !defined(UNICODE)
+#define UNICODE
+#endif
+
+#include <tchar.h>
+#include <windows.h>
+#include <cmath>
+/*  Declare Windows procedure  */
+LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
+
+/*  Make the class name into a global variable  */
+TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
+
+int WINAPI WinMain (HINSTANCE hThisInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nCmdShow)
+{
+    HWND hwnd;               /* This is the handle for our window */
+    MSG messages;            /* Here messages to the application are saved */
+    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* The Window structure */
+    wincl.hInstance = hThisInstance;
+    wincl.lpszClassName = szClassName;
+    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
+    wincl.style = CS_DBLCLKS;                 /* Catch double-clicks */
+    wincl.cbSize = sizeof (WNDCLASSEX);
+
+    /* Use default icon and mouse-pointer */
+    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
+    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wincl.lpszMenuName = NULL;                 /* No menu */
+    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
+    wincl.cbWndExtra = 0;                      /* structure or the window instance */
+    /* Use Windows's default colour as the background of the window */
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+
+    /* Register the window class, and if it fails quit the program */
+    if (!RegisterClassEx (&wincl))
+        return 0;
+
+    /* The class is registered, let's create the program*/
+    hwnd = CreateWindowEx (
+               0,                   /* Extended possibilites for variation */
+               szClassName,         /* Classname */
+               _T("Code::Blocks Template Windows App"),       /* Title Text */
+               WS_OVERLAPPEDWINDOW, /* default window */
+               CW_USEDEFAULT,       /* Windows decides the position */
+               CW_USEDEFAULT,       /* where the window ends up on the screen */
+               544,                 /* The programs width */
+               375,                 /* and height in pixels */
+               HWND_DESKTOP,        /* The window is a child-window to desktop */
+               NULL,                /* No menu */
+               hThisInstance,       /* Program Instance handler */
+               NULL                 /* No Window Creation data */
+           );
+
+    /* Make the window visible on the screen */
+    ShowWindow (hwnd, nCmdShow);
+
+    /* Run the message loop. It will run until GetMessage() returns 0 */
+    while (GetMessage (&messages, NULL, 0, 0))
+    {
+        /* Translate virtual-key messages into character messages */
+        TranslateMessage(&messages);
+        /* Send message to WindowProcedure */
+        DispatchMessage(&messages);
+    }
+
+    /* The program return-value is 0 - The value that PostQuitMessage() gave */
+    return messages.wParam;
+}
+
+union OutCode
+{
+    unsigned All:4;
+    struct
+    {
+        unsigned left:1,top:1,right:1,bottom:1;
+    };
+};
+
+
+OutCode GetOutCode(double x,double y,int xleft,int ytop,int xright,int ybottom)
+{
+    OutCode out;
+    out.All=0;
+    if(x<xleft)out.left=1;
+    else if(x>xright)out.right=1;
+    if(y<ytop)out.top=1;
+    else if(y>ybottom)out.bottom=1;
+    return out;
+}
+void VIntersect(double xs,double ys,double xe,double ye,int x,double *xi,double *yi)
+{
+    *xi=x;
+    *yi=ys+(x-xs)*(ye-ys)/(xe-xs);
+}
+void HIntersect(double xs,double ys,double xe,double ye,int y,double *xi,double *yi)
+{
+    *yi=y;
+    *xi=xs+(y-ys)*(xe-xs)/(ye-ys);
+}
+void CohenSuth(HDC hdc,int xs,int ys,int xe,int ye,int xleft,int ytop,int xright,int ybottom)
+{
+    double x1=xs,y1=ys,x2=xe,y2=ye;
+    OutCode out1=GetOutCode(x1,y1,xleft,ytop,xright,ybottom);
+    OutCode out2=GetOutCode(x2,y2,xleft,ytop,xright,ybottom);
+    while( (out1.All || out2.All) && !(out1.All & out2.All))
+    {
+        double xi,yi;
+        if(out1.All)
+        {
+            if(out1.left)VIntersect(x1,y1,x2,y2,xleft,&xi,&yi);
+            else if(out1.top)HIntersect(x1,y1,x2,y2,ytop,&xi,&yi);
+            else if(out1.right)VIntersect(x1,y1,x2,y2,xright,&xi,&yi);
+            else HIntersect(x1,y1,x2,y2,ybottom,&xi,&yi);
+            x1=xi;
+            y1=yi;
+            out1=GetOutCode(x1,y1,xleft,ytop,xright,ybottom);
+        }
+        else
+        {
+            if(out2.left)VIntersect(x1,y1,x2,y2,xleft,&xi,&yi);
+            else if(out2.top)HIntersect(x1,y1,x2,y2,ytop,&xi,&yi);
+            else if(out2.right)VIntersect(x1,y1,x2,y2,xright,&xi,&yi);
+            else HIntersect(x1,y1,x2,y2,ybottom,&xi,&yi);
+            x2=xi;
+            y2=yi;
+            out2=GetOutCode(x2,y2,xleft,ytop,xright,ybottom);
+        }
+    }
+    if(!out1.All && !out2.All)
+    {
+        MoveToEx(hdc,round(x1),round(y1),NULL);
+        LineTo(hdc,round(x2),round(y2));
+    }
+}
+/*  This function is called by the Windows function DispatchMessage()  */
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static int xs, ys, xe, ye; // coordinates of the line to be clipped
+    static int xleft, ytop, xright, ybottom; // clipping rectangle
+    HDC hdc;
+    PAINTSTRUCT ps;
+    HPEN hPen ;
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+
+        hdc = BeginPaint(hwnd, &ps);
+        hPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0)); // Create a red pen
+
+        // Define the line to be clipped
+        xs = 50, ys = 50, xe = 200, ye = 200;
+
+        // Define the clipping rectangle
+        xleft = 100, ytop = 100, xright = 300, ybottom = 300;
+
+        // Draw the unclipped line
+        MoveToEx(hdc, xs, ys, NULL);
+        LineTo(hdc, xe, ye);
+
+        // Clip the line using the Cohen-Sutherland algorithm
+        CohenSuth(hdc, xs, ys, xe, ye, xleft, ytop, xright, ybottom);
+
+        // Cleanup
+        DeleteObject(hPen);
+        EndPaint(hwnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, message, wParam, lParam);
+    }
+    return 0;
+}
